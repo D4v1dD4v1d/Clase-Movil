4d6744f7d97b586f384edeaf222d268c9e11e8aa
D4v1dD4v1d <david.pz.zv@gmail.com>
2023-10-02 15:11:12 -0600

    Mejorar formato de la tarea

--- Notes/app/src/main/java/com/example/notes/MainActivity.kt
+++ Notes/app/src/main/java/com/example/notes/MainActivity.kt
@@ -7,18 +7,23 @@ import androidx.activity.compose.setContent
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.sizeIn
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -96,7 +101,7 @@ fun notaCard(nota: Nota, modifier: Modifier=Modifier){
                     text = nota.nombre
                 )
                 Text(
-                    text = nota.descripcion
+                    text = nota.descripcion.substring(0,20)+"..."
                 )
                 Text(
                     text = nota.fecha.toString()
@@ -104,8 +109,7 @@ fun notaCard(nota: Nota, modifier: Modifier=Modifier){
             }
         }
     }
-}
-@Composable
+}@Composable
 fun taskCard(tarea: Tarea, modifier: Modifier=Modifier){
     Card (
         elevation = CardDefaults.cardElevation(defaultElevation = 2.dp),
@@ -119,41 +123,48 @@ fun taskCard(tarea: Tarea, modifier: Modifier=Modifier){
                 .padding(16.dp)
                 .sizeIn(minHeight = 72.dp)
         ) {
-            Column(modifier = Modifier.weight(1f)) {
-                Text(
-                    text = tarea.nombre
-                )
-                Text(
-                    text = tarea.descripcion
-                )
-                Text(
-                    text = tarea.fecha.toString()
-                )
-                Text(
-                    text = tarea.realizada.toString()
-                )
+            val checked = remember { mutableStateOf(false)}
+            Column(modifier = Modifier.fillMaxWidth(0.9f)) {
+                Row {
+                    Text(
+                        text = tarea.nombre
+                    )
+                }
+                Row {
+                    Text(
+                        text = tarea.descripcion.substring(0,20)+"..."
+                    )
+                }
+                Row {
+                    Text(
+                        text = tarea.fecha.toString()
+                    )
+                }
+            }
+
+            Column(modifier = Modifier.fillMaxWidth(0.1f)){
+                Row {
+
+                }
+                Row {
+                    Checkbox(
+                        checked=checked.value,
+                        onCheckedChange = {isChecked ->checked.value=isChecked}
+                    )
+                }
+
             }
         }
-    }
-}
-/*
-@RequiresApi(Build.VERSION_CODES.O)
-@Preview(showBackground = true)
-@Composable
-fun ShowCard() {
-    NotesTheme {
-        val nota: Nota=notasInfo.notas[0];
-        notaCard(nota)
     }
 }
 
 @RequiresApi(Build.VERSION_CODES.O)
 @Preview(showBackground = true)
 @Composable
-fun ShowTask() {
-    NotesTheme{
-        val tarea: Tarea=tareasInfo.tareas[0];
-        notaCard(tarea)
+fun ShowCard() {
+    NotesTheme {
+        val nota: Tarea=tareasInfo.tareas[0];
+        taskCard(nota)
     }
 }
-*/
+
