52891fe6e548fcae0ce064ef9eca444b28b866af
D4v1dD4v1d <david.pz.zv@gmail.com>
2023-10-24 16:45:04 -0600

    .

--- Navegacion dinamica/app/src/main/java/com/example/reply/ui/ReplyApp.kt
+++ Navegacion dinamica/app/src/main/java/com/example/reply/ui/ReplyApp.kt
@@ -15,12 +15,15 @@
  */
 package com.example.reply.ui
 
+import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.reply.data.Email
 import com.example.reply.data.MailboxType
+import com.example.reply.ui.utils.ReplyNavigationType
+
 
 @Composable
 fun ReplyApp(
@@ -28,8 +31,10 @@ fun ReplyApp(
 ) {
     val viewModel: ReplyViewModel = viewModel()
     val replyUiState = viewModel.uiState.collectAsState().value
+    val navigationType =ReplyNavigationType.PERMANENT_NAVIGATION_DRAWER;
 
     ReplyHomeScreen(
+
         replyUiState = replyUiState,
         onTabPressed = { mailboxType: MailboxType ->
             viewModel.updateCurrentMailbox(mailboxType = mailboxType)
@@ -43,6 +48,23 @@ fun ReplyApp(
         onDetailScreenBackPressed = {
             viewModel.resetHomeScreenStates()
         },
-        modifier = modifier
+        modifier = modifier,
+        navigationType = when (windowSize) {
+            WindowWidthSizeClass.Compact -> {
+                navigationType = ReplyNavigationType.BOTTOM_NAVIGATION
+            }
+
+            WindowWidthSizeClass.Medium -> {
+                navigationType = ReplyNavigationType.NAVIGATION_RAIL
+            }
+
+            WindowWidthSizeClass.Expanded -> {
+                navigationType = ReplyNavigationType.PERMANENT_NAVIGATION_DRAWER
+            }
+
+            else -> {
+                navigationType = ReplyNavigationType.BOTTOM_NAVIGATION
+            }
+        }
     )
 }
